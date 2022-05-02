dc1453abe305034c9a6685a9ad745a063c67de2d
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-05-02 19:55:44 -0300

    REVISAO COMPLETA LING JAVA

---
+++ src/RevisaoConceitoJAVA/EntradaSaidaDeDados.java
@@ -0,0 +1,27 @@
+package RevisaoConceitoJAVA;
+
+import java.util.Locale;
+
+public class EntradaSaidaDeDados {
+
+	public static void main(String[] args) {
+		
+		String nome = "Maria";
+		int idade = 31;
+		double renda = 4000.0;
+		double x = 10.35784;
+		
+		System.out.println(x);
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f%n", x);
+		
+		System.out.println("RESULTADO = " + x + " METROS");
+		System.out.printf("RESULTADO = %.2f metros%n", x);
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+		
+	}
+
+}
