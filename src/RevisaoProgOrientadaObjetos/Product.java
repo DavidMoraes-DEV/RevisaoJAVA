4ef9446a5c5e3f4451f39ede9f2725ff0441beab
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-05-30 20:05:56 -0300

    Revisao do conceito de Construtor

--- src/RevisaoProgOrientadaObjetos/Product.java
+++ src/RevisaoProgOrientadaObjetos/Product.java
@@ -1,4 +1,4 @@
-package RevisaoProgOrientadaObjetos.ProblemaExProdutoEmEstoque;
+package RevisaoProgOrientadaObjetos;
 
 public class Product {
 
@@ -6,34 +6,32 @@ public class Product {
 	public String name;
 	public double price;
 	public int quantity;
+	
+	//Construtor
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
 
-	//Métodos:
-	public double TotalValueInStock() {
-		
-		return price * quantity;			
+	//Métodos
+	public double totalValueInStock() {
+		return price * quantity;
 	}
-	
-	public void AddProducts(int qtd) {
-		
-		this.quantity += qtd;
+
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
 	}
-	
-	public void RemoveProducts(int qtd) {
-		
-		this.quantity -= qtd;
+
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
 	}
-	
-	@Override
+
 	public String toString() {
-		return "Nome: "
-				+ name
-				+ ", Preço R$: "
-				+ String.format("%.2f", price)
-				+ ", Quantidade: "
-				+ quantity
-				+ " Total: R$: "
-				+ String.format("%.2f", this.TotalValueInStock());
+		return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ "
+				+ String.format("%.2f", totalValueInStock());
 	}
+
 }
 /*
  * CONSTRUTOR: - É uma operação especial da classe, que executa no momenta da
