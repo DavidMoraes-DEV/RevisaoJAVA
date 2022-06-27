b7262a0933ec875a0d15dd22b81f86102e489ce6
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-06-26 23:15:59 -0300

    Revisao basica de DEFAULT METHODS das interfaces parte I

---
+++ src/RevisaoProgOrientadaObjetos/Interfaces/DefaultMethods/Problematica.java
@@ -0,0 +1,15 @@
+package RevisaoProgOrientadaObjetos.Interfaces.DefaultMethods;
+
+public class Problematica {
+}
+/*
+* DEFAULT METHODS (DEFENDER METHODS)
+ 	- A partir do JAVA 8, Interfaces podem conter métodos concretos. (Dependendo do caso expecífico podem ter métodos estaticos e métodos privados)
+ 	- A intenção básica é prover implementação padrão para métodos, de modo a evitar:
+ 		- Repetiçao de implementação em toda classe que implemente a interface
+ 		- A necessidade de se criar classes abstratas para prover reuso da implementação
+ 	
+ 	- Outras Vantagens:
+ 		- Manter a retrocompatibilidade com sistemas existentes
+ 		- Permitir que "interfaces funcionais" (que devem conter apenas um método) possam prover outras operações padrão reutilizáveis		
+*/
